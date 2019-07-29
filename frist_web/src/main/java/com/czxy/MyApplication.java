677f0f1f2c65c71f4210b6ff0d7db43875fd10aa
1714145080@qq.com <1714145080@qq.com>
2019-07-29 17:50:16 +0800

    first_obj

---
+++ frist_web/src/main/java/com/czxy/MyApplication.java
@@ -0,0 +1,15 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version lt 1.0
+ * @date 2019/7/29
+ */
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class,args);
+    }
+}
